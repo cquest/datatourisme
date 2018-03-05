907269360511d815efc2ce3dc43e9409496c1550
cquest <cquest@openstreetmap.fr>
2018-03-05 14:56:03 +0100

    premier jet

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # datatourisme
 Scripts de traitement des données DATAtourisme
+
+## Conversion en CSV simplifié
+
+Afin de faciliter la réutilisation, les principales données sémantiques sont converties en fichier CSV "plat".
---
+++ jsonld2csv.py
@@ -0,0 +1,62 @@
+import sys
+import json
+import csv
+import re
+
+
+def ldget(obj, path, default=None):
+    "Descente dans le graphe de données"
+    if obj is not None and path[0] in obj:
+        if len(path) == 1:
+            return obj[path[0]]
+        else:
+            return ldget(obj[path[0]], path[1:], default=default)
+    else:
+        return default
+
+
+# fichier CSV de sortie
+with open(re.sub(r'json.*', 'csv', sys.argv[1]), 'w') as csvfile:
+    fields = ['id', 'label', 'type', 'theme', 'startdate', 'enddate',
+              'street', 'postalcode', 'city', 'insee',
+              'latitude', 'longitude', 'lastupdate', 'comment']
+    csv_out = csv.writer(csvfile)
+    csv_out.writerow(fields)
+
+    # fichier json-ld en entrée
+    with open(sys.argv[1], 'r') as json_file:
+        data = json.load(json_file)
+        for e in data['@graph']:
+            # extraction des éléments du graphe à sortir en CSV
+            uri = ldget(e, ['@id'])
+            label = ldget(e, ['rdfs:label'])
+            comment = ldget(e, ['rdfs:comment'])
+            startdate = ldget(e, [':takesPlaceAt', ':startDate', '@value'])
+            enddate = ldget(e, [':takesPlaceAt', ':endDate', '@value'])
+            geo = ldget(e, [':isLocatedAt', 'schema:geo'])
+            lat = ldget(geo, ['schema:latitude', '@value'])
+            lon = ldget(geo, ['schema:longitude', '@value'])
+            addr = ldget(e, [':isLocatedAt', 'schema:address'])
+            street = ldget(addr, ['schema:streetAddress'])
+            cp = ldget(addr, ['schema:postalCode'], '')
+            city = ldget(addr, [':hasAddressCity', 'rdfs:label'])
+            if city:
+                city = city[0]['@value']
+            insee = ldget(addr, [':hasAddressCity', ':insee'])
+            last_update = ldget(addr, [':lastUpdate'])
+            event_type = '/'.join(ldget(e, ['@type']))
+
+            themes = ldget(e, [':hasTheme', 'rdfs:label'], None)
+            event_theme = ''
+            if themes:
+                print(themes)
+                for t in themes:
+                    if t['@language'] == 'fr':
+                        event_theme = event_theme + t['@value'] + ', '
+                        print(t['@value'])
+                event_theme = event_theme[:-2]
+
+            # écriture dans le fichier CSV
+            csv_out.writerow([uri, label, event_type, event_theme, startdate,
+                              enddate, street, cp, city, insee, lat, lon,
+                              last_update, comment])
